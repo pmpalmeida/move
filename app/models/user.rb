60d1617bffde0da9cbb4bd58eae5f4c4fca0e422
pedro almeida <pedro.marc2003@gmail.com>
2017-05-09 19:47:42 -0300

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+
+rails generate migration AddNameToUsers name:string
+rails generate migration Addis_femaleToUsers is_female:boolean, default: false
+rails generate migration AddAdminToUsers admin:boolean, default: false
