98e2883f735101f87c35e0ea5214b5935819c7b1
pedro almeida <pedro.marc2003@gmail.com>
2017-05-10 12:41:37 -0300

    views para user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
   def show
+
     @user = User.find(params[:id])
 
-    if !@user.pprofile_id.nil?
-      @pprofile = @user.pprofile_id
+    if current_user == @user
+      if !@user.pprofile_id.nil?
+        @pprofile = @user.pprofile_id
+      end
+    else
+      redirect_to root_path
     end
-
   end
 
 end
