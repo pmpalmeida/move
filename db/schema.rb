93774f3f368c9c79ade239ce7341f7c7ab9b7eff
pedro almeida <pedro.marc2003@gmail.com>
2017-05-10 09:44:46 -0300

    model Pprofile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509225224) do
+ActiveRecord::Schema.define(version: 20170510124311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pprofiles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "phone_number"
+    t.string   "city"
+    t.string   "site"
+    t.string   "instagram"
+    t.string   "twitter"
+    t.string   "forum"
+    t.string   "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
