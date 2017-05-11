3438b2de67904fbd7f5c9cae0183dcf7d0d0eb35
pedro almeida <pedro.marc2003@gmail.com>
2017-05-11 11:20:32 -0300

    view para edicao de perfil de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [ :show ]
   resources :cities, only: [ :show ]
-  resources :pprofiles, only: [ :update, :destroy ]
+  resources :pprofiles, only: [ :update, :destroy, :show ]
 
 
 
