f9e2c575ae24058c249eac41e1662d9cab167644
pedro almeida <pedro.marc2003@gmail.com>
2017-06-14 11:17:48 -0300

    modificacao pre feriado

--- app/controllers/pprofiles_controller.rb
+++ app/controllers/pprofiles_controller.rb
@@ -7,7 +7,7 @@ class PprofilesController < ApplicationController
   def update
     @pprofile = Pprofile.find(params[:id])
     @pprofile.update(pprofile_params)
-    redirect_to root_path
+    redirect_to payments_path
   end
 
 end
