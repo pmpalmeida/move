905c8083d23d0d5814a105c709f3a56719bd5eae
pedro almeida <pedro.marc2003@gmail.com>
2017-06-14 22:47:42 -0300

    aws photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
 
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ roles: [:is_female] }, :email, :password, :password_confirmation, :name)
+      user_params.permit({ roles: [:is_female] }, :email, :password, :password_confirmation, :name, :photo, :photo_cache)
     end
 
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
+  mount_uploader :photo, UserPhotoUploader
+
   has_one :pprofile, dependent: :destroy
   has_many :citypresences
   has_many :cities, through: :citypresences
