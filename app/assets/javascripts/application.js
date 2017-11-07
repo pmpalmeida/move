2f34395db339c3fddb041dc8053b3ee80e860b37
pedro almeida <pedro.marc2003@gmail.com>
2017-11-07 18:53:48 -0200

    carousel responsivo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,47 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+//= require jquery.slick
+
+
+
+
+$('.header-carousel').slick({
+  centerMode: true,
+  centerPadding: '1vw',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
